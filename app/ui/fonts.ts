9f275314f938b9be91b8b2372af02b0a3faf7b08
tatsiana_hlebik <tatsiana_hlebik@example.com>
2025-07-14 17:11:39 +0300

    start the project

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Barlow, Lusitana } from 'next/font/google';
+
+export const barlow = Barlow({ 
+    subsets: ['latin'],
+    weight: ['400', '700', '900'] 
+ });
+
+ export const lusitana = Lusitana ({
+    subsets: ['latin'],
+    weight: ['400', '700']
+ })
